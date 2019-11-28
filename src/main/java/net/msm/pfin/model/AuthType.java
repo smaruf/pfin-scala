f48f2756d6626a32110fa5d9e087f6e61a47bf8b
Muhammad Shamsul Maruf <smaruf00ruet320@gmail.com>
2019-11-28 12:31:34 +0600

    Authentication-Type enumeration  2019.11.28.1231

---
+++ src/main/java/net/msm/pfin/model/AuthType.java
@@ -0,0 +1,8 @@
+@Gettter
+@AllArgConstructor
+public enum AuthenticationType {
+    DEFAULT(0, "Default"),
+    SINGLE_SIGN_ON(0, "Single Signing");
+    private Long id;
+    private String name;
+}
